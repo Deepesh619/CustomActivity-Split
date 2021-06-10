edb196c93e8a7f5e6ab6349eeeb829728584603a
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-06-10 15:11:59 +0530

    CHanges in prod

--- public/js/CustomActivity.js
+++ public/js/CustomActivity.js
@@ -1,37 +1,34 @@
 var connection = new Postmonger.Session();
 var eventDefinitionKey;
 var payload={};
+
 connection.trigger('ready');
+
+ // Below event is executed any time and is used to get the event definition key
+connection.trigger('requestTriggerEventDefinition');
+connection.on('requestedTriggerEventDefinition',
+function(eventDefinitionModel) {
+   if(eventDefinitionModel){
+    console.log('eventDefinitionModel : '+ JSON.stringify(eventDefinitionModel));
+     eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+     getEntrySourceColumnList(eventDefinitionModel.dataExtensionId);
+   }
+});
+
 connection.on('initActivity',function(data){
     console.log(data);
     if (data) {
         payload = data;
     }
     document.getElementById('srcColumnName').value= payload['arguments'].execute.inArguments[0].srcColumnName;
-    document.getElementById('destDEName').value= payload['arguments'].execute.inArguments[0].destDEName;
     document.getElementById('destMappedCol').value= payload['arguments'].execute.inArguments[0].destMappedCol;
     document.getElementById('destCompCol').value= payload['arguments'].execute.inArguments[0].destCompCol;
     document.getElementById('destCompVal').value= payload['arguments'].execute.inArguments[0].destCompVal;
-    getDEList();
-    document.getElementById('destDEName').value= payload['arguments'].execute.inArguments[0].destDEName;
+    getDEList();    
  }); 
-
-
-
- // Below event is executed any and is used to get the event definition key
-
-connection.trigger('requestTriggerEventDefinition');
-connection.on('requestedTriggerEventDefinition',
-function(eventDefinitionModel) {
-   if(eventDefinitionModel){
-     eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
-   }
-}); 
-
 // Below event is executed when Done is clicked on UI
 
 connection.on('clickedNext',save);
-
 
 // save function is used to save the content from the UI
 
@@ -65,6 +62,55 @@ function getDEList(){
           for(var index in obj) {
           select.options[select.options.length] = new Option(obj[index], index);// new Option(text-DEName, value-CustomerKey)
           }
+          document.getElementById('destDEName').value= payload['arguments'].execute.inArguments[0].destDEName;
+          if(document.getElementById('destDEName').selectedIndex >= 0){
+          getColumnList(document.getElementById('destDEName'));}
+      }
+  }
+  http.send(data); 
+}
+
+function getColumnList(option){
+  var http = new XMLHttpRequest();
+  var ID = option.value;
+  var url = 'https://mcservicecall-dev.herokuapp.com/MCService/getColumnList?ID='+ID+ '&DEName=true';
+  var data = new FormData();
+  http.open('GET', url);
+  http.onreadystatechange = function() {//Call a function when the state changes.
+      if(http.readyState == 4 && http.status == 200) {
+          var obj = {};
+          obj = JSON.parse(this.responseText);
+          console.log(obj);
+          var select = document.getElementById("destMappedCol");
+          select.innerHTML = "";
+          for(var index in obj) {
+          select.options[select.options.length] = new Option(obj[index], obj[index]);
+          }
+          document.getElementById('destMappedCol').value= payload['arguments'].execute.inArguments[0].destMappedCol;
+          document.getElementById('destCompCol').innerHTML= select.innerHTML;
+          document.getElementById('destCompCol').value= payload['arguments'].execute.inArguments[0].destCompCol;
+      }
+  }
+  http.send(data); 
+}
+
+function getEntrySourceColumnList(objectID){
+  var http = new XMLHttpRequest();
+  var ID = objectID;
+  var url = 'https://mcservicecall-dev.herokuapp.com/MCService/getColumnList?ID='+ID + '&DEName=false';
+  var data = new FormData();
+  http.open('GET', url);
+  http.onreadystatechange = function() {//Call a function when the state changes.
+      if(http.readyState == 4 && http.status == 200) {
+          var obj = {};
+          obj = JSON.parse(this.responseText);
+          console.log(obj);
+          var select = document.getElementById("srcColumnName");
+          select.innerHTML = "";
+          for(var index in obj) {
+          select.options[select.options.length] = new Option(obj[index], obj[index]);
+          }
+          document.getElementById('srcColumnName').value= payload['arguments'].execute.inArguments[0].srcColumnName;
       }
   }
   http.send(data); 
